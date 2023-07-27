609c8bd2f1168064aa3bfbfb67cf346c182afa1b
Sruthi S <129876043+2149-SRUTHI-S@users.noreply.github.com>
2023-07-27 12:02:16 +0530

    Add files via upload

---
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundAdvice.java
@@ -0,0 +1,22 @@
+package com.sruthi.FullStackCRUD.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class UserNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String,String> exceptionHandler(UserNotFoundException exception){
+
+        Map<String,String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage",exception.getMessage());
+
+        return errorMap;
+    }
+}
---
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sruthi.FullStackCRUD.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(Long id) {
+        super("Could not found the user with ID " + id);
+    }
+}
