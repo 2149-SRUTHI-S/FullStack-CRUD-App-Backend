c1d831be83e2f0e3b10be648d6b7a86aed17472a
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2023-12-09 13:00:19 +0530

    Update FullStackCrudApplication.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/FullStackCrudApplication.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/FullStackCrudApplication.java
@@ -2,7 +2,7 @@ package com.sruthi.FullStackCRUD;
        
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-     
+      
 @SpringBootApplication
 public class FullStackCrudApplication {
 
