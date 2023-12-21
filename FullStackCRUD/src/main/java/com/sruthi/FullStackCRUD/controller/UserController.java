6278ab7809d9ecffac76f77402248df197755d54
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2023-12-21 20:37:17 +0530

    Update UserController.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/controller/UserController.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/controller/UserController.java
@@ -1,5 +1,5 @@
 package com.sruthi.FullStackCRUD.controller;
-vvv 
+vv
 import com.sruthi.FullStackCRUD.exception.UserNotFoundException;
 import com.sruthi.FullStackCRUD.model.User;
 import com.sruthi.FullStackCRUD.repository.UserRepository;
