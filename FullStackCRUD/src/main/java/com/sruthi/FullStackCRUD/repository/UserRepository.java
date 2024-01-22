88841b9562a27a9c940fa3e617713a4f48d2d075
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-01-22 20:54:51 +0530

    Update UserRepository.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/repository/UserRepository.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.sruthi.FullStackCRUD.repository;
      
 import com.sruthi.FullStackCRUD.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;  
+import org.springframework.data.jpa.repository.JpaRepository;       
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
