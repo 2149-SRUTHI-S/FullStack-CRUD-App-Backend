150d2c50b9ee200e2978d1767db1b54d450a54da
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-01-28 20:44:14 +0530

    Update UserNotFoundException.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundException.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.sruthi.FullStackCRUD.exception;     
    
 public class UserNotFoundException extends RuntimeException{
-  
+    
     public UserNotFoundException(Long id) {
         super("Could not found the user with ID " + id);
     }
