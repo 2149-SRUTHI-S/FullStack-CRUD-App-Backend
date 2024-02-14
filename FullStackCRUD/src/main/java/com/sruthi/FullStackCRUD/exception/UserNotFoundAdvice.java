89012bd84c382e395e66e7350db6146bcc273a79
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-02-14 22:48:06 +0530

    Update UserNotFoundAdvice.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundAdvice.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/exception/UserNotFoundAdvice.java
@@ -1,4 +1,4 @@
-package com.sruthi.FullStackCRUD.exception;
+package com.sruthi.FullStackCRUD.exception;  
                    
 import org.springframework.http.HttpStatus;            
 import org.springframework.web.bind.annotation.*;  
