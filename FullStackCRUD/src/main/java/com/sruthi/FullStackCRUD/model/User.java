8f56175a59e8b62e4df09a5f688b58c77b60287f
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-02-29 21:58:13 +0530

    Update User.java

--- FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/model/User.java
+++ FullStackCRUD/src/main/java/com/sruthi/FullStackCRUD/model/User.java
@@ -1,5 +1,5 @@
 package com.sruthi.FullStackCRUD.model;          
-                                                 
+                                                   
                                                          
 import javax.persistence.Entity;   
 import javax.persistence.GeneratedValue; 
